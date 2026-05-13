e2f3a9b14ee52d7c95bc9678cbc103b0aea9edbe
caioikegami <cikegami@gmail.com>
2026-05-13 15:39:47 -0300

    Atualizado API

--- scripts.js
+++ scripts.js
@@ -1,51 +1,52 @@
 const convertButton = document.querySelector('.convert-button')
 const currencySelect = document.querySelector('.currency-select')
 
-function convertValues() {
+async function convertValues() {
     const inputCurrencyValue = document.querySelector('.input-currency').value
     const currencyValueToConvert = document.querySelector('.currency-value-to-convert') // Valor em Real //
     const currencyValueConverted = document.querySelector('.currency-value') // outras moedas //
 
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL,JPY-BRL,CNY-BRL").then(response => response.json())
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
-    const libraToday = 6.9
-    const ieneToday = 0.03
-    const yuanToday = 0.76
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const libra = data.GBPBRL.high
+    const iene = data.JPYBRL.high
+    const yuan = data.CNYBRL.high
 
     if (currencySelect.value == 'dolar') {
         currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD'
-        }).format(inputCurrencyValue / dolarToday)
+        }).format(inputCurrencyValue / dolar)
     }
 
     if (currencySelect.value == 'euro') {
         currencyValueConverted.innerHTML = new Intl.NumberFormat('de-DE', {
             style: 'currency',
             currency: 'EUR'
-        }).format(inputCurrencyValue / euroToday)
+        }).format(inputCurrencyValue / euro)
     }
 
     if (currencySelect.value == 'libra') {
         currencyValueConverted.innerHTML = new Intl.NumberFormat('en-GB', {
             style: 'currency',
             currency: 'GBP'
-        }).format(inputCurrencyValue / libraToday)
+        }).format(inputCurrencyValue / libra)
     }
 
-     if (currencySelect.value == 'iene') {
+    if (currencySelect.value == 'iene') {
         currencyValueConverted.innerHTML = new Intl.NumberFormat('ja-JP', {
             style: 'currency',
             currency: 'JPY'
-        }).format(inputCurrencyValue / ieneToday)
+        }).format(inputCurrencyValue / iene)
     }
 
-      if (currencySelect.value == 'yuan') {
+    if (currencySelect.value == 'yuan') {
         currencyValueConverted.innerHTML = new Intl.NumberFormat('zh-CN', {
             style: 'currency',
             currency: 'CNY'
-        }).format(inputCurrencyValue / ieneToday)
+        }).format(inputCurrencyValue / yuan)
     }
 
 
@@ -59,29 +60,29 @@ function changeCurrency() {
     const currencyName = document.getElementById('currency-name')
     const currencyImage = document.querySelector('.currency-img')
 
-    if(currencySelect.value == 'dolar'){
-       currencyName.innerHTML = 'Dólar Americano'
-       currencyImage.src = './assets/dolar.png' 
+    if (currencySelect.value == 'dolar') {
+        currencyName.innerHTML = 'Dólar Americano'
+        currencyImage.src = './assets/dolar.png'
     }
 
-     if(currencySelect.value == 'euro'){
-       currencyName.innerHTML = 'Euro'
-       currencyImage.src = './assets/euro.png'  
+    if (currencySelect.value == 'euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImage.src = './assets/euro.png'
     }
 
-    if(currencySelect.value == 'libra'){
-       currencyName.innerHTML = 'Libra Esterlina'
-       currencyImage.src = './assets/libra.png'  
+    if (currencySelect.value == 'libra') {
+        currencyName.innerHTML = 'Libra Esterlina'
+        currencyImage.src = './assets/libra.png'
     }
 
-     if(currencySelect.value == 'iene'){
-       currencyName.innerHTML = 'Iene Japones'
-       currencyImage.src = './assets/yen.png'  
+    if (currencySelect.value == 'iene') {
+        currencyName.innerHTML = 'Iene Japones'
+        currencyImage.src = './assets/yen.png'
     }
 
-     if(currencySelect.value == 'yuan'){
-       currencyName.innerHTML = 'CN¥ Yuan Chinês'
-       currencyImage.src = './assets/yuan.png'  
+    if (currencySelect.value == 'yuan') {
+        currencyName.innerHTML = 'CN¥ Yuan Chinês'
+        currencyImage.src = './assets/yuan.png'
     }
 
     convertValues()
